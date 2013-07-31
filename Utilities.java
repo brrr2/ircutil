583c97458985746b981980c0f9681c86f4cda648
Yizhe Shen <brrr@live.ca>
2013-07-31 17:27:27 -0400

    Added "away" and "back" commands.
    Implemented the commands by creating a file called "away.txt" in the run directory. The file stores the list of hostmasks of users who do not wish to be highlighted by the "ping" command. Added an Op command "clearaway" to empty this file from time to time.

--- Utilities.java
+++ Utilities.java
@@ -1,5 +1,11 @@
 package ircutil;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.*;
 import org.pircbotx.*;
 
@@ -38,6 +44,14 @@ public class Utilities{
                     bot.sendNotice(user, bot.getNick()+" must be joined to at least 1 channel.");
                 }
             }
+        // Erases all hostmasks from away.txt
+        } else if (command.equals("clearaway")){
+            if (!channel.isOp(user)){
+                bot.sendNotice(user, "You do not have Op privileges in this channel.");
+            } else {
+                clearAwayList();
+                bot.sendNotice(user, "The away list as been emptied.");
+            }
         // Display current host time
         } else if (command.equals("time")){
             bot.sendMessage(channel, "Time: " + new Date().toString());
@@ -58,15 +72,37 @@ public class Utilities{
                 outStr += ((Channel)it.next()).getName()+", ";
             }
             bot.sendMessage(channel, outStr.substring(0, outStr.length()-2));
-        // Display a list of users in a channel excluding ChanServ and the bot
+        // Remove the user from the away list
+        } else if (command.equals("back")){
+            ArrayList<String> awayList = getAwayList();
+            if (awayList.contains(user.getHostmask())){
+                setUserBack(awayList, user.getHostmask());
+                bot.sendNotice(user, "You are no longer marked as away.");
+            } else {
+                bot.sendNotice(user, "You are not marked as away!");
+            }
+        // Add the user to the away list
+        } else if (command.equals("away")){
+            ArrayList<String> awayList = getAwayList();
+            if (awayList.contains(user.getHostmask())){
+                bot.sendNotice(user, "You are already marked as away!");
+            } else {
+                setUserAway(user.getHostmask());
+                bot.sendNotice(user, "You are now marked as away.");
+            }
+        // Display a list of users in a channel excluding ChanServ, the bot
         } else if (command.equals("ping")){
+            ArrayList<String> awayList = getAwayList();
             Set<User> users = bot.getUsers(channel);
+            User tUser;
             String tNick;
             String outStr = "Ping: ";
             Iterator<User> it = users.iterator();
             while(it.hasNext()){
-                tNick= ((User)it.next()).getNick();
-                if (!tNick.equals("ChanServ") && !tNick.equals(bot.getNick())){
+                tUser = (User)it.next();
+                tNick = tUser.getNick();
+                if (!tNick.equals("ChanServ") && !tNick.equals(bot.getNick()) &&
+                        !awayList.contains(tUser.getHostmask())){
                     outStr += tNick+", ";
                 }
             }
@@ -86,10 +122,53 @@ public class Utilities{
             bot.sendMessage(channel, "Hi "+user.getNick()+"!");
         // Displays a list of commands
         } else if (command.equals("commands")){
-            bot.sendMessage(channel, "Commands: channels, time, uptime, ping, coin, hi, help");
+            bot.sendMessage(channel, "Commands: channels, time, uptime, away, back, ping, coin, hi, help");
         // Displays a help message
         } else if (command.equals("help")){
             bot.sendMessage(channel, user.getNick()+": Type .ghelp");
         }
 	}
+    
+    private static void setUserAway(String hostmask){
+        try {
+            PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("away.txt",true)));
+            out.println(hostmask);
+            out.close();
+        } catch (IOException e) {
+            // do nothing
+        }
+    }
+    private static void setUserBack(ArrayList<String> awayList, String hostmask){
+        try {
+            PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("away.txt")));
+            awayList.remove(hostmask);
+            for (int ctr = 0; ctr < awayList.size(); ctr++){
+                out.println(awayList.get(ctr));
+            }
+            out.close();
+        } catch (IOException e) {
+            // do nothing
+        }
+    }
+    private static void clearAwayList(){
+        try {
+            PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("away.txt")));
+            out.close();
+        } catch (IOException e) {
+            // do nothing
+        }
+    }
+    private static ArrayList<String> getAwayList(){
+        try {
+            ArrayList<String> hostmasks = new ArrayList<String>();
+            BufferedReader in = new BufferedReader(new FileReader("away.txt"));
+            while (in.ready()) {
+                hostmasks.add(in.readLine());
+            }
+            in.close();
+            return hostmasks;
+        } catch (IOException e){
+            return new ArrayList<String>();
+        }      
+    }
 }
